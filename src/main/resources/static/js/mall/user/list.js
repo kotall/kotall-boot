42c2e1eacae8309e09f8a3f67aa00cfe3d871c6c
aracwong <aracwong@163.com>
2018-11-13 11:26:01 +0800

    添加litemall 用户中心功能

--- src/main/resources/static/js/mall/user/list.js
+++ src/main/resources/static/js/mall/user/list.js
@@ -24,20 +24,12 @@ function getGrid() {
 		columns: [
 			{checkbox: true},
 			{field : "username", title : "用户名称", width : "100px"}, 
-			{field : "password", title : "用户密码", width : "100px"}, 
-			{field : "gender", title : "性别：0 未知， 1男， 1 女", width : "100px"}, 
+			{field : "gender", title : "性别", width : "100px"},
 			{field : "birthday", title : "生日", width : "100px"}, 
-			{field : "lastLoginTime", title : "最近一次登录时间", width : "100px"}, 
-			{field : "lastLoginIp", title : "最近一次登录IP地址", width : "100px"}, 
-			{field : "userLevel", title : "0 普通用户，1 VIP用户，2 高级VIP用户", width : "100px"}, 
+			{field : "userLevel", title : "用户等级", width : "100px"},
 			{field : "nickname", title : "用户昵称或网络名称", width : "100px"}, 
-			{field : "mobile", title : "用户手机号码", width : "100px"}, 
-			{field : "avatar", title : "用户头像图片", width : "100px"}, 
-			{field : "weixinOpenid", title : "微信登录openid", width : "100px"}, 
-			{field : "status", title : "0 可用, 1 禁用, 2 注销", width : "100px"}, 
-			{field : "addTime", title : "创建时间", width : "100px"}, 
-			{field : "updateTime", title : "更新时间", width : "100px"}, 
-			{field : "deleted", title : "逻辑删除", width : "100px"}
+			{field : "status", title : "用户状态", width : "100px"},
+			{field : "addTime", title : "创建时间", width : "100px"}
 		]
 	})
 }
@@ -55,8 +47,8 @@ var vm = new Vue({
 			dialogOpen({
 				title: '新增用户表',
 				url: 'mall/user/add.html?_' + $.now(),
-				width: '420px',
-				height: '350px',
+				width: '500px',
+				height: '500px',
 				yes : function(iframeId) {
 					top.frames[iframeId].vm.acceptClick();
 				},
@@ -68,8 +60,8 @@ var vm = new Vue({
 				dialogOpen({
 					title: '编辑用户表',
 					url: 'mall/user/edit.html?_' + $.now(),
-					width: '420px',
-					height: '350px',
+					width: '500px',
+					height: '500px',
 					success: function(iframeId){
 						top.frames[iframeId].vm.liteMallUser.id = ck[0].id;
 						top.frames[iframeId].vm.setForm();
