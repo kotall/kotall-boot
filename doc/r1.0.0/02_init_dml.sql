479582e28c1c16f13d651ded05d6d0d38667385d
aracwong <aracwong@163.com>
2018-11-13 16:04:21 +0800

    菜单模块重构

--- doc/r1.0.0/02_init_dml.sql
+++ doc/r1.0.0/02_init_dml.sql
@@ -3,7 +3,7 @@
 -- 目录SQL
 INSERT INTO `sys_menu` (`parent_id`, `name`, `code`,`url`, `perms`, `type`, `icon`, `order_num`) VALUES (0, '用户中心', 'LITE_MALL_UC',NULL, NULL, '0', 'fa fa-coffee', 2);
 -- 菜单SQL
-INSERT INTO `sys_menu` (`parent_id`, `name`, `code`,`url`, `perms`, `type`, `icon`, `order_num`) VALUES ((select m.menu_id from sys_menu m where m.code='LITE_MALL_UC_USER'), '用户管理', 'LITE_MALL_USER_','mall/user/list.html', NULL, '1', 'fa fa-th-list', 1);
+INSERT INTO `sys_menu` (`parent_id`, `name`, `code`,`url`, `perms`, `type`, `icon`, `order_num`) VALUES ((select m.menu_id from sys_menu m where m.code='LITE_MALL_UC'), '用户管理', 'LITE_MALL_UC_USER','mall/user/list.html', NULL, '1', 'fa fa-th-list', 1);
 -- 菜单对应按钮SQL
 INSERT INTO `sys_menu` (`parent_id`, `name`, `code`,`perms`, `type`, `icon`) VALUES ((select m.menu_id from sys_menu m where m.code='LITE_MALL_UC_USER'), '刷新', 'LITE_MALL_UC_USER_LIST', 'litemall:user:list', '2', NULL);
 INSERT INTO `sys_menu` (`parent_id`, `name`, `code`,`perms`, `type`, `icon`) VALUES ((select m.menu_id from sys_menu m where m.code='LITE_MALL_UC_USER'), '新增', 'LITE_MALL_UC_USER_SAVE', 'litemall:user:save', '2', NULL);
@@ -19,7 +19,7 @@ INSERT INTO `sys_menu` (`parent_id`, `name`, `code`,`perms`, `type`, `icon`) VAL
 INSERT INTO `sys_menu` (`parent_id`, `name`, `code`,`perms`, `type`, `icon`) VALUES ((select m.menu_id from sys_menu m where m.code='LITE_MALL_UC_ADDRESS'), '修改', 'LITE_MALL_UC_ADDRESS_EDIT', 'litemall:address:edit', '2', NULL);
 INSERT INTO `sys_menu` (`parent_id`, `name`, `code`,`perms`, `type`, `icon`) VALUES ((select m.menu_id from sys_menu m where m.code='LITE_MALL_UC_ADDRESS'), '删除', 'LITE_MALL_UC_ADDRESS_REMOVE', 'litemall:address:remove', '2', NULL);
 
-INSERT INTO `sys_menu` (`parent_id`, `name`, `code`,`url`, `perms`, `type`, `icon`, `order_num`) VALUES ((select m.menu_id from sys_menu m where m.code='/LITE_MALL_UC'), '会员收藏', 'LITE_MALL_UC_COLLECT','mall/collect/list.html', NULL, '1', 'fa fa-th-list', 3);
+INSERT INTO `sys_menu` (`parent_id`, `name`, `code`,`url`, `perms`, `type`, `icon`, `order_num`) VALUES ((select m.menu_id from sys_menu m where m.code='LITE_MALL_UC'), '会员收藏', 'LITE_MALL_UC_COLLECT','mall/collect/list.html', NULL, '1', 'fa fa-th-list', 3);
 -- 菜单对应按钮SQL
 INSERT INTO `sys_menu` (`parent_id`, `name`, `code`,`perms`, `type`, `icon`) VALUES ((select m.menu_id from sys_menu m where m.code='LITE_MALL_UC_COLLECT'), '刷新', 'LITE_MALL_UC_COLLECT_LIST', 'litemall:collect:list', '2', NULL);
 INSERT INTO `sys_menu` (`parent_id`, `name`, `code`,`perms`, `type`, `icon`) VALUES ((select m.menu_id from sys_menu m where m.code='LITE_MALL_UC_COLLECT'), '新增', 'LITE_MALL_UC_COLLECT_SAVE', 'litemall:collect:save', '2', NULL);
@@ -28,7 +28,7 @@ INSERT INTO `sys_menu` (`parent_id`, `name`, `code`,`perms`, `type`, `icon`) VAL
 
 
 -- 菜单SQL
-INSERT INTO `sys_menu` (`parent_id`, `name`, `code`,`url`, `perms`, `type`, `icon`, `order_num`) VALUES ((select m.menu_id from sys_menu m where m.code='/LITE_MALL_UC'), '会员足迹', 'LITE_MALL_UC_FOOTPRINT','mall/footprint/list.html', NULL, '1', 'fa fa-th-list', 4);
+INSERT INTO `sys_menu` (`parent_id`, `name`, `code`,`url`, `perms`, `type`, `icon`, `order_num`) VALUES ((select m.menu_id from sys_menu m where m.code='LITE_MALL_UC'), '会员足迹', 'LITE_MALL_UC_FOOTPRINT','mall/footprint/list.html', NULL, '1', 'fa fa-th-list', 4);
 -- 菜单对应按钮SQL
 INSERT INTO `sys_menu` (`parent_id`, `name`, `code`,`perms`, `type`, `icon`) VALUES ((select m.menu_id from sys_menu m where m.code='LITE_MALL_UC_FOOTPRINT'), '刷新', 'LITE_MALL_UC_FOOTPRINT_LIST', 'litemall:footprint:list', '2', NULL);
 INSERT INTO `sys_menu` (`parent_id`, `name`, `code`,`perms`, `type`, `icon`) VALUES ((select m.menu_id from sys_menu m where m.code='LITE_MALL_UC_FOOTPRINT'), '新增', 'LITE_MALL_UC_FOOTPRINT_SAVE', 'litemall:footprint:save', '2', NULL);
@@ -37,7 +37,7 @@ INSERT INTO `sys_menu` (`parent_id`, `name`, `code`,`perms`, `type`, `icon`) VAL
 
 
 -- 菜单SQL
-INSERT INTO `sys_menu` (`parent_id`, `name`, `code`,`url`, `perms`, `type`, `icon`, `order_num`) VALUES ((select m.menu_id from sys_menu m where m.code='/LITE_MALL_UC'), '搜索历史', 'LITE_MALL_UC_HISTORY','mall/history/list.html', NULL, '1', 'fa fa-th-list', 5);
+INSERT INTO `sys_menu` (`parent_id`, `name`, `code`,`url`, `perms`, `type`, `icon`, `order_num`) VALUES ((select m.menu_id from sys_menu m where m.code='LITE_MALL_UC'), '搜索历史', 'LITE_MALL_UC_HISTORY','mall/history/list.html', NULL, '1', 'fa fa-th-list', 5);
 -- 菜单对应按钮SQL
 INSERT INTO `sys_menu` (`parent_id`, `name`, `code`,`perms`, `type`, `icon`) VALUES ((select m.menu_id from sys_menu m where m.code='LITE_MALL_UC_HISTORY'), '刷新', 'LITE_MALL_UC_HISTORY_LIST', 'litemall:history:list', '2', NULL);
 INSERT INTO `sys_menu` (`parent_id`, `name`, `code`,`perms`, `type`, `icon`) VALUES ((select m.menu_id from sys_menu m where m.code='LITE_MALL_UC_HISTORY'), '新增', 'LITE_MALL_UC_HISTORY_SAVE', 'litemall:history:save', '2', NULL);
@@ -46,7 +46,7 @@ INSERT INTO `sys_menu` (`parent_id`, `name`, `code`,`perms`, `type`, `icon`) VAL
 
 
 -- 菜单SQL
-INSERT INTO `sys_menu` (`parent_id`, `name`, `code`,`url`, `perms`, `type`, `icon`, `order_num`) VALUES ((select m.menu_id from sys_menu m where m.code='/LITE_MALL_UC'), '意见反馈', 'LITE_MALL_UC_FEEDBACK','mall/feedback/list.html', NULL, '1', 'fa fa-th-list', 6);
+INSERT INTO `sys_menu` (`parent_id`, `name`, `code`,`url`, `perms`, `type`, `icon`, `order_num`) VALUES ((select m.menu_id from sys_menu m where m.code='LITE_MALL_UC'), '意见反馈', 'LITE_MALL_UC_FEEDBACK','mall/feedback/list.html', NULL, '1', 'fa fa-th-list', 6);
 -- 菜单对应按钮SQL
 INSERT INTO `sys_menu` (`parent_id`, `name`, `code`,`perms`, `type`, `icon`) VALUES ((select m.menu_id from sys_menu m where m.code='LITE_MALL_UC_FEEDBACK'), '刷新', 'LITE_MALL_UC_FEEDBACK_LIST', 'litemall:feedback:list', '2', NULL);
 INSERT INTO `sys_menu` (`parent_id`, `name`, `code`,`perms`, `type`, `icon`) VALUES ((select m.menu_id from sys_menu m where m.code='LITE_MALL_UC_FEEDBACK'), '新增', 'LITE_MALL_UC_FEEDBACK_SAVE', 'litemall:feedback:save', '2', NULL);
