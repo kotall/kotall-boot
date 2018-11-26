67e9a224f382d359163c075ae7afae6544235169
aracwong <aracwong@163.com>
2018-11-26 12:06:22 +0800

    升级spring-boot 版本至 2.1.0.RELEASE

--- src/main/java/com/kotall/rms/core/config/ApiConfig.java
+++ src/main/java/com/kotall/rms/core/config/ApiConfig.java
@@ -10,13 +10,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Configuration
-public class ApiConfig extends WebMvcConfigurerAdapter {
+public class ApiConfig implements WebMvcConfigurer {
 
     @Autowired
     private LiteMallAppService appService;
@@ -31,18 +31,18 @@ public class ApiConfig extends WebMvcConfigurerAdapter {
      */
     @Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-        //1.构建了一个HttpMessageConverter  FastJson   消息转换器
+        // 1.构建了一个HttpMessageConverter  FastJson   消息转换器
         FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
-        //2.定义一个配置，设置编码方式，和格式化的形式
+        // 2.定义一个配置，设置编码方式，和格式化的形式
         FastJsonConfig fastJsonConfig = new FastJsonConfig();
-        //3.设置成了PrettyFormat格式
+        // 3.设置成了PrettyFormat格式
         fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat);
-        //4.处理中文乱码问题
+        // 4.处理中文乱码问题
         List<MediaType> fastMediaTypes =  new ArrayList<>();
         fastMediaTypes.add(MediaType.APPLICATION_JSON_UTF8);
         fastConverter.setSupportedMediaTypes(fastMediaTypes);
 
-        //5.将fastJsonConfig加到消息转换器中
+        // 5.将fastJsonConfig加到消息转换器中
         fastConverter.setFastJsonConfig(fastJsonConfig);
         converters.add(fastConverter);
     }
