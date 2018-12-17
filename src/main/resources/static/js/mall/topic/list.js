5102e6fce435e8919309c78380a27773a5c6dae4
aracwong <aracwong@163.com>
2018-12-17 20:54:01 +0800

    初始化

--- src/main/resources/static/js/mall/topic/list.js
+++ src/main/resources/static/js/mall/topic/list.js
@@ -26,12 +26,17 @@ function getGrid() {
 			{field : "storeName", title : "店铺", width : "120px"},
 			{field : "title", title : "专题标题", width : "100px"},
 			{field : "subtitle", title : "专题子标题", width : "100px"},
-			{field : "content", title : "专题内容，富文本格式", width : "100px"}, 
-			{field : "price", title : "专题相关商品最低价", width : "100px"}, 
+			{field : "content", title : "专题内容", width : "100px",
+				formatter: function (value, index, row) {
+				    // 弹出层
+					return value;
+                }
+			},
+			{field : "price", title : "商品低价", width : "100px"},
 			{field : "readCount", title : "专题阅读量", width : "100px"}, 
 			{field : "picUrl", title : "专题图片", width : "100px"}, 
 			{field : "sortOrder", title : "排序", width : "100px"}, 
-			{field : "goods", title : "专题相关商品，采用JSON数组格式", width : "100px"}, 
+			// {field : "goods", title : "专题相关商品，采用JSON数组格式", width : "100px"},
 			{field : "addTime", title : "创建时间", width : "120px"}
 		]
 	})
@@ -50,8 +55,8 @@ var vm = new Vue({
 			dialogOpen({
 				title: '新增专题表',
 				url: 'mall/topic/add.html?_' + $.now(),
-				width: '420px',
-				height: '350px',
+				width: '700px',
+				height: '620px',
 				yes : function(iframeId) {
 					top.frames[iframeId].vm.acceptClick();
 				},
@@ -63,8 +68,8 @@ var vm = new Vue({
 				dialogOpen({
 					title: '编辑专题表',
 					url: 'mall/topic/edit.html?_' + $.now(),
-					width: '420px',
-					height: '350px',
+					width: '700px',
+					height: '620px',
 					success: function(iframeId){
 						top.frames[iframeId].vm.liteMallTopic.id = ck[0].id;
 						top.frames[iframeId].vm.setForm();
